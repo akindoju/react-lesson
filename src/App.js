5b2329fb2809ce1ee67e3a0ac0f81cb08200544b
Akndju <davidd.akindoju@gmail.com>
2021-02-22 19:37:21 +0100

    Introduced routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 import SignIn from './components/signin/sign';
+import MainPage from './components/mainpage/mainpage';
+import { Route, Switch } from 'react-router';
 
 function App() {
   return (
     <div className="App">
-      <SignIn />
+      <Switch>
+        <Route path="/" exact>
+          <SignIn />
+        </Route>
+        <Route path="/mainpage" exact>
+          <MainPage />
+        </Route>
+      </Switch>
     </div>
   );
 }
