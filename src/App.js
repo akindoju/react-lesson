6c3be77a6715c77c09eec13369a8427acb6ecd3b
Akndju <davidd.akindoju@gmail.com>
2021-02-25 19:03:40 +0100

    Integrated redirect into project

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
 import './App.css';
 import SignIn from './components/signin/sign';
 import MainPage from './components/mainpage/mainpage';
-import { Route, Switch } from 'react-router';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-function App() {
+function App(props) {
+  const { user } = props;
   return (
     <div className="App">
       <Switch>
-        <Route path="/" exact>
-          <SignIn />
-        </Route>
-        <Route path="/mainpage" exact>
-          <MainPage />
-        </Route>
+        <Route
+          path="/"
+          exact
+          render={() => (!user ? <SignIn /> : <Redirect to="/mainpage" />)}
+        />
+        <Route
+          path="/mainpage"
+          exact
+          render={() => (user ? <MainPage /> : <Redirect to="/" />)}
+        />
       </Switch>
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  user: state.user.currentUser,
+});
+
+export default connect(mapStateToProps)(App);
--- src/components/mainpage/mainpage.jsx
+++ src/components/mainpage/mainpage.jsx
@@ -1,35 +1,47 @@
 import React from 'react';
+import { logout } from '../../redux/user/user.actions';
+import { FormButton } from '../signin/signin.styles';
 import './mainpage.css';
+import { useDispatch } from 'react-redux';
 
 const MainPage = () => {
+  const dispatch = useDispatch();
   return (
     <div className="imageContainer">
+      <FormButton
+        onClick={(event) => {
+          event.preventDefault();
+          dispatch(logout());
+        }}
+      >
+        Sign out
+      </FormButton>
       <img
-        src={`https://i.pinimg.com/236x/b0/b6/fd/b0b6fda959abd3c5d6a887c6a97b811e.jpg`}
+        src={`https://i.pinimg.com/originals/4b/3a/a8/4b3aa811d845485d39d9709d963ba020.jpg`}
       />
       <img
-        src={`https://i.pinimg.com/236x/b0/b6/fd/b0b6fda959abd3c5d6a887c6a97b811e.jpg`}
+        src={`https://images.unsplash.com/photo-1504253492562-cbc4dc540fcb?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8cm9hZCUyMGFlc3RoZXRpY3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80`}
       />
       <img
-        src={`https://i.pinimg.com/236x/b0/b6/fd/b0b6fda959abd3c5d6a887c6a97b811e.jpg`}
+        src={`https://media2.architecturemedia.net/site_media/media/cache/05/22/0522e323da93abbffe7aa29db8ac4caf.jpg`}
       />
       <img
-        src={`https://i.pinimg.com/236x/b0/b6/fd/b0b6fda959abd3c5d6a887c6a97b811e.jpg`}
+        src={`https://media2.architecturemedia.net/site_media/media/cache/05/22/0522e323da93abbffe7aa29db8ac4caf.jpg`}
       />
       <img
-        src={`https://i.pinimg.com/236x/b0/b6/fd/b0b6fda959abd3c5d6a887c6a97b811e.jpg`}
+        src={`https://media2.architecturemedia.net/site_media/media/cache/05/22/0522e323da93abbffe7aa29db8ac4caf.jpg`}
       />
       <img
-        src={`https://i.pinimg.com/236x/b0/b6/fd/b0b6fda959abd3c5d6a887c6a97b811e.jpg`}
+        src={`https://media2.architecturemedia.net/site_media/media/cache/05/22/0522e323da93abbffe7aa29db8ac4caf.jpg`}
       />
       <img
-        src={`https://i.pinimg.com/236x/b0/b6/fd/b0b6fda959abd3c5d6a887c6a97b811e.jpg`}
+        src={`https://media2.architecturemedia.net/site_media/media/cache/05/22/0522e323da93abbffe7aa29db8ac4caf.jpg`}
       />
       <img
-        src={`https://i.pinimg.com/236x/b0/b6/fd/b0b6fda959abd3c5d6a887c6a97b811e.jpg`}
+        src={`https://media2.architecturemedia.net/site_media/media/cache/05/22/0522e323da93abbffe7aa29db8ac4caf.jpg`}
       />
       <img
-        src={`https://i.pinimg.com/236x/b0/b6/fd/b0b6fda959abd3c5d6a887c6a97b811e.jpg`}
+        src={`https://media2.architecturemedia.net/site_media/media/cache/05/22/0522e323da93abbffe7aa29db8ac4caf.jpg`}
       />
     </div>
   );
