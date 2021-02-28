3c3b75f30e7361e7d7fa1beb877a217a876ceda9
Akndju <davidd.akindoju@gmail.com>
2021-02-28 17:05:29 +0100

    Finalized project

--- src/components/signin/signin.test.js
+++ src/components/signin/signin.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import SignIn from './signin';
 
 describe('SignIn testing', () => {
@@ -11,5 +12,20 @@ describe('SignIn testing', () => {
   it('contains password placeholder', () => {
     render(<SignIn />);
     screen.getByPlaceholderText(`Enter Password`);
+  });
+
+  it('sends correct credentials', async () => {
+    const mockLogin = jest.fn();
+    const { getByPlaceholderText, getByText } = render(
+      <SignIn login={mockLogin} errorMessage={''} />
+    );
+    const usernameField = getByPlaceholderText('Enter Username');
+    const passwordField = getByPlaceholderText('Enter Password');
+
+    await userEvent.type(usernameField, 'Shayo');
+    await userEvent.type(passwordField, '1234');
+    await userEvent.click(getByText('Submit'));
+
+    expect(mockLogin).toBeCalledWith('Shayo', '1234');
   });
 });
